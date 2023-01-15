7a26f1acf215dfbe9132e69f5ba14a79ac534a38
Dr. Jochen Manns <manns@insynergie.de>
2023-01-15 12:56:37 +0100

    chore: dist gehört nichts ins GIT.

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ValidationError, ValidationSchema } from 'fastest-validator';
-export interface IModelControl {
-    findErrors(prop?: string): ValidationError[];
-    isDirty(): boolean;
-    reset(): void;
-}
-export declare function useModel<T extends object>(original: T, schema?: ValidationSchema): [T, IModelControl];
